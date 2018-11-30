dc5f2a2b77eb4c9ff932336705e6c13253fefd55
truelatysh <sinyakov@phystech.edu>
2018-12-01 01:25:28 +0300

    Update dz_2.py
    Проверка типов с type, это абсолютно легально, но есть еще вариант с isinstance, он в промышленном коде чаще используется.
    С ифами намутил, куча лишних условий, исправил.
    Вынес инпуты за пределы сигнатуры функции, так более правильно. Хотя в общем случае, иметь условия в сигнатуре норм.

--- dz_2.py
+++ dz_2.py
@@ -1,15 +1,15 @@
 def isstring(string_1, string_2):
-    if type(string_1) != str or type(string_2) != str:
+    if not (isinstance(string_1, str) and isinstance(string_2, str)):
         return '0'
     # print(type(string_1), type(string_2))
-    else:
-        if string_1 == string_2:
-            return '1'
-        else:
-            if string_1 != string_2 and len(string_1) > len(string_2):
-                return '2'
-            elif string_1 != string_2 and string_2 == 'learn':
-                return '3'
+    elif string_1 == string_2:
+        return '1'
+    elif len(string_1) > len(string_2):
+        return '2'
+    elif string_2 == 'learn':
+        return '3'
 
-result = isstring(string_1 = input('Напишите что-нибудь: '), string_2 = input('Напишите еще что-нибудь: '))
+
+string_1, string_2 = input('Напишите что-нибудь: '), input('Напишите еще что-нибудь: ')
+result = isstring(string_1, string_2)
 print(result)
